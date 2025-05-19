81e59f5d86865e2ab5602a657dab1c97909c45d2
Saurav Khare <saurav.erahk@gmail.com>
2025-05-19 11:18:22 +0530

    feat: updated global styles - added neobrutalism styles in global styles - added neobrutalism button and input components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="h-screen flex justify-center items-center my-7">
 			<h1>bitroad begins!</h1>
-			<Button>Init!</Button>
+			<Button variant="reverse">I am a button</Button>
+			<Input placeholder="I am an input"></Input>
 		</div>
 	);
 }
