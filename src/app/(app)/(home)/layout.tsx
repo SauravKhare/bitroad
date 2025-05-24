f68229dee67aa05153d28f56e77187e04c8be806
Saurav Khare <saurav.erahk@gmail.com>
2025-05-25 02:42:53 +0530

    feat: added category menu, search UI

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,10 +1,40 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+import { Category } from "@/payload-types";
+
 import { Footer } from "@/components/layout/footer/Footer";
 import { Navbar } from "@/components/layout/navbar/Navbar";
+import { SearchFilters } from "@/components/layout/search-filters/SearchFilters";
 
-const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+const HomeLayout = async ({
+	children,
+}: Readonly<{ children: React.ReactNode }>) => {
+	const payload = await getPayload({
+		config: configPromise,
+	});
+
+	const data = await payload.find({
+		collection: "categories",
+		depth: 1,
+		pagination: false,
+		where: {
+			parent: {
+				exists: false,
+			},
+		},
+	});
+
+	const categories = data.docs.map((doc) => ({
+		...doc,
+		subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+			...(doc as Category),
+		})),
+	}));
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Navbar />
+			<SearchFilters data={categories} />
 			<div className="flex-1 bg-subtle-bg">{children}</div>
 			<Footer />
 		</div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,19 +1,5 @@
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
-
 const Home = async () => {
-	const payload = await getPayload({
-		config: configPromise,
-	});
-
-	const data = await payload.find({
-		collection: "categories",
-	});
-	return (
-		<div className="flex justify-center items-center my-7">
-			{JSON.stringify(data, null, 2)}
-		</div>
-	);
+	return <div className="flex justify-center items-center my-7"></div>;
 };
 
 export default Home;
